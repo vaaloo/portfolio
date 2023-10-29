8931f0158427977a40bb82259889f24eb8a563a0
vaaloo <lesrageursdu13@gmail.com>
2023-10-29 19:57:36 +0100

    Added header, and parallax effect

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,10 +1,20 @@
 import Link from 'next/link'
 import styles from './Header.module.css'
 import { usePathname } from 'next/navigation'
+import React, { useEffect } from 'react';
+import Parallax from 'parallax-js';
+
 
 export default function Header() {
     const currentRoute = usePathname();
+    useEffect(() => {
+        const scene = new Parallax(document.getElementById('p'), {
+            relativeInput: true,
+            limitY: 0,
+        });
+      }, []);
     return (
+        <>
         <header className={styles.header}>
             <div className={styles.logo_container}>
                 <div className={styles.logo}></div>
@@ -16,5 +26,14 @@ export default function Header() {
                 <Link href="/contact" className={currentRoute === "/contact" ? styles.active : ""}>Contact</Link>
             </nav>
         </header>
+        <section className={styles.sub_header} id="p">
+                <div className={styles.points} data-depth="0.1"></div>
+                <div className={styles.rounds} data-depth="0.2"></div>
+                <svg data-depth="0.1" className={styles.v} id="v" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 505 530">
+                    <path className={styles.vcolor} d="M252.5,530L0,0H142l181.5,383-71,147Z"/>
+                    <path className={styles.vcolor}d="M346.5,334l-71-149.5L363,1h142l-158.5,333Z"/>
+                </svg>
+        </section>
+        </>
     )
 }
