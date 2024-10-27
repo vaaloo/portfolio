9f0d44dddbc0aeadadaf2b49e1bbb66d2b107fb0
vaaloo <lesrageursdu13@gmail.com>
2024-10-27 09:59:16 +0100

    who am i updated

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,17 @@ export default function Home() {
         <div className={styles.content_container}>
             <div className={styles.desc_container}>
                 <h2>Who am I ?</h2>
-                <p>I&apos;m a young <b>fullstack web developer</b>, still a student. I&apos;ve had several <b>projects</b> in my development career, which you can find on my <Link href="/projects">projects</Link> page.
-                    I started coding with <b>NodeJS</b>, then gradually learned <b>HTML and CSS. </b>
+                <p>I&apos;m a young <b>fullstack web developer</b>, still a student. I&apos;ve had
+                    several <b>projects</b> in my development career, which you can find on my <Link
+                        href="/projects">projects</Link> page.
+                    I started coding with <b>NodeJS</b>, then gradually learned <b>HTML and CSS</b>.
                     More recently, I&apos;ve started using <b>NextJS</b> (this site being made with this).
-                    I was able to learn <b>C++</b> during my studies too, although it&apos;s not much use to me for the web!
+                    <br/>I also had the opportunity to code <b>trading bots</b> and explore <b>Java</b> during my studies,
+                    adding to my development skills.
+                    I was able to learn <b>C++</b> too, although it&apos;s not much use to me for the web !
                     If you&apos;d like to <b>contact me</b>, please go to my <Link href="/contact">contact</Link> page !
                 </p>
+
             </div>
             <div className={styles.skills_all_container}>
                 <h2>My skills</h2>
